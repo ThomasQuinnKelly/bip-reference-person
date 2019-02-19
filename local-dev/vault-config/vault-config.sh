f62c857e8662a8b6f9ae3e258999f938c8c63014
jasonluck <jluck@meetveracity.com>
2019-02-19 13:10:35 -0500

    Adding vault and consul configuration for local dev

---
+++ local-dev/vault-config/vault-config.sh
@@ -0,0 +1,33 @@
+#! /bin/sh
+echo "Configuring Vault..."
+
+# Wait up to 60 seconds for Vault to be available
+until $(curl -XGET --insecure --fail --output /dev/null --silent -H "X-Vault-Token: $VAULT_TOKEN" $VAULT_ADDR/sys/health); do
+    echo "Waiting for Vault to be available..."
+    sleep 10
+done
+
+# Authenticate to Vault
+vault login $VAULT_DEV_ROOT_TOKEN_ID
+
+#################### Enable Consul Secret Backend ####################
+vault secrets enable consul
+
+# Retreive ACL token from Consul
+ACL_TOKEN=$(curl -ks \
+    --header "X-Consul-Token: ${CONSUL_HTTP_TOKEN}" \
+    --request PUT \
+    --data '{"Name": "sample", "Policies": [{"name": "global-management"}]}' \
+    ${CONSUL_HTTP_ADDR}/v1/acl/token | jq -r .SecretID)
+
+vault write consul/config/access \
+    address=${CONSUL_HTTP_ADDR} \
+    token=${ACL_TOKEN}
+
+# Create Token Policy
+consul acl policy create  -name "readonly" -description "Read Only Policy" -rules @/token-policy.hcl
+
+# Configure Role Mapping
+vault write consul/roles/os-svc policies=readonly
+
+######################################################################
