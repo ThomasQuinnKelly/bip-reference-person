8b52d6ebddbb90b1e3fe1101c95b6d51b4630757
Abhijit Kulkarni <akulkarni@meetveracity.com>
2019-02-21 13:12:22 -0500

    clean up of service methods for SSN that aren't needed

--- os-reference-service-1/src/main/java/gov/va/os/reference/service/api/DemoService.java
+++ os-reference-service-1/src/main/java/gov/va/os/reference/service/api/DemoService.java
@@ -1,6 +1,5 @@
 package gov.va.os.reference.service.api;
 
-import java.util.concurrent.Future;
 
 import gov.va.os.reference.service.api.v1.transfer.DemoServiceResponse;
 
@@ -13,12 +12,4 @@ public interface DemoService {
    * @return the demo service response
    */
 	DemoServiceResponse read(String name);
-	
-	/**
-	 * Reads the DemoServiceResponse.
-	 *
-	 * @param id Read Name
-	 * @return the future
-	 */
-	Future<DemoServiceResponse> readAsync(String id);
 }
