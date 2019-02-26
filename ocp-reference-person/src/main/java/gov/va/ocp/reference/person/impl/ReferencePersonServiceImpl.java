eb42848be0ba5e27c0683ea667d3096f0387f22f
armin <aburkholder@meetveracity.com>
2019-02-26 09:57:30 -0500

    CMAPI2-36 update service impl

--- ocp-reference-person/src/main/java/gov/va/ocp/reference/person/impl/ReferencePersonServiceImpl.java
+++ ocp-reference-person/src/main/java/gov/va/ocp/reference/person/impl/ReferencePersonServiceImpl.java
@@ -20,18 +20,12 @@ import gov.va.ocp.reference.framework.exception.ReferenceRuntimeException;
 import gov.va.ocp.reference.framework.messages.Message;
 import gov.va.ocp.reference.framework.messages.MessageSeverity;
 import gov.va.ocp.reference.framework.util.Defense;
-import gov.va.ocp.reference.partner.person.ws.client.PersonWsClient;
-import gov.va.ocp.reference.partner.person.ws.transfer.FindPersonByPtcpntId;
-import gov.va.ocp.reference.partner.person.ws.transfer.FindPersonByPtcpntIdResponse;
 import gov.va.ocp.reference.partner.person.ws.transfer.ObjectFactory;
-import gov.va.ocp.reference.partner.person.ws.transfer.PersonDTO;
 import gov.va.ocp.reference.person.api.ReferencePersonService;
-import gov.va.ocp.reference.person.exception.PersonServiceException;
-import gov.va.ocp.reference.person.model.person.v1.PersonInfo;
 import gov.va.ocp.reference.person.model.person.v1.PersonInfoRequest;
 import gov.va.ocp.reference.person.model.person.v1.PersonInfoResponse;
 import gov.va.ocp.reference.person.utils.HystrixCommandConstants;
-import gov.va.ocp.reference.person.utils.StringUtil;
+import gov.va.ocp.reference.person.ws.client.PersonServiceHelper;
 
 @Service(value = ReferencePersonServiceImpl.BEAN_NAME)
 @Component
@@ -55,9 +49,9 @@ public class ReferencePersonServiceImpl implements ReferencePersonService {
 	/** Bean name constant */
 	public static final String BEAN_NAME = "personServiceImpl";
 
-	/** The person web service client reference. */
+	/** The person web service client helper. */
 	@Autowired
-	private PersonWsClient personWsClient;
+	private PersonServiceHelper personServiceHelper;
 
 	@Autowired
 	private CacheManager cacheManager;
@@ -89,23 +83,16 @@ public class ReferencePersonServiceImpl implements ReferencePersonService {
 	public PersonInfoResponse findPersonByParticipantID(final PersonInfoRequest personInfoRequest) {
 
 		// Check for valid input arguments and WS Client reference.
-		Defense.notNull(personWsClient, "Unable to proceed with Person Service request. The personWsClient must not be null.");
+		Defense.notNull(personServiceHelper,
+				"Unable to proceed with Person Service request. The personServiceHelper must not be null.");
 		Defense.notNull(personInfoRequest.getParticipantID(), "Invalid argument, pid must not be null.");
 
 		if (cacheManager.getCache(CACHENAME_REFERENCE_PERSON_SERVICE) != null
 				&& cacheManager.getCache(CACHENAME_REFERENCE_PERSON_SERVICE).get(personInfoRequest) != null) {
 			return cacheManager.getCache(CACHENAME_REFERENCE_PERSON_SERVICE).get(personInfoRequest, PersonInfoResponse.class);
 		} else {
-			// Prepare the WS request
-			final FindPersonByPtcpntId findPersonByPtcpntIdRequestElement = createFindPersonByPidRequest(personInfoRequest);
-// TODO
-//			// Invoke the Person Web Service via the WS Client
-//			final JAXBElement<FindPersonByPtcpntIdResponse> findPersonByPtcpntIdResponseElement =
-//					personWsClient.getPersonInfoByPtcpntId(findPersonByPtcpntIdRequestElement);
-//
-//			// Prepare the service response
-//			return createPersonInfoResponse(findPersonByPtcpntIdResponseElement, personInfoRequest.getParticipantID());
-			return null;
+			PersonInfoResponse response = personServiceHelper.findPersonByPid(personInfoRequest);
+			return response;
 		}
 	}
 
@@ -152,76 +139,4 @@ public class ReferencePersonServiceImpl implements ReferencePersonService {
 		msg.setText(text);
 		return msg;
 	}
-
-	/**
-	 * @param personInfoRequest The request from the Java Service.
-	 * @return A JAXB element for the WS request
-	 */
-	private FindPersonByPtcpntId createFindPersonByPidRequest(final PersonInfoRequest personInfoRequest) {
-
-		final FindPersonByPtcpntId findPersonByPtcpntId = new FindPersonByPtcpntId();
-		findPersonByPtcpntId.setPtcpntId(personInfoRequest.getParticipantID());
-
-//		TODO
-//		return PERSON_OBJECT_FACTORY.createFindPersonByPtcpntId(findPersonByPtcpntId);
-		return null;
-	}
-
-	/**
-	 * @param findPersonByPtcpntIdResponseElement The response JAXB element
-	 * @param participantID The person's participantID
-	 * @return the person info response
-	 */
-	private PersonInfoResponse createPersonInfoResponse(
-			final FindPersonByPtcpntIdResponse findPersonByPtcpntIdResponseElement, final Long participantID) {
-
-		final PersonInfoResponse personInfoResponse = new PersonInfoResponse();
-		final String maskedInfo = StringUtil.getMask4(participantID.toString());
-		// Check for null xml element
-		if (findPersonByPtcpntIdResponseElement == null) {
-
-			personInfoResponse.addMessage(MessageSeverity.ERROR, NOPERSONFORPTCTID,
-					NO_PERSON_FOUND_FOR_PARTICIPANT_ID + maskedInfo);
-
-		} else {
-			final FindPersonByPtcpntIdResponse findPersonByPtcpntIdResponse = findPersonByPtcpntIdResponseElement;
-			// Check for null response object
-			if (findPersonByPtcpntIdResponse == null) {
-
-				personInfoResponse.addMessage(MessageSeverity.ERROR, NOPERSONFORPTCTID,
-						NO_PERSON_FOUND_FOR_PARTICIPANT_ID + maskedInfo);
-
-			} else {
-				final PersonDTO personDto = findPersonByPtcpntIdResponse.getPersonDTO();
-				// If no DTO was returned, the PID did not match a person.
-				if (personDto == null) {
-
-					personInfoResponse.addMessage(MessageSeverity.ERROR, NOPERSONFORPTCTID,
-							NO_PERSON_FOUND_FOR_PARTICIPANT_ID + maskedInfo);
-
-				} else {
-					// Copy the data of interest to the person info and add it to the service response.
-					final PersonInfo personInfo = createPersonInfo(personDto);
-					personInfoResponse.setPersonInfo(personInfo);
-				}
-			}
-		}
-		return personInfoResponse;
-	}
-
-	/**
-	 * @param personDto The person DTO
-	 * @return A PersonInfo object containing data from the DTO
-	 */
-	private PersonInfo createPersonInfo(final PersonDTO personDto) {
-		final PersonInfo personInfo = new PersonInfo();
-		personInfo.setFileNumber(personDto.getFileNbr());
-		personInfo.setFirstName(personDto.getFirstNm());
-		personInfo.setMiddleName(personDto.getMiddleNm());
-		personInfo.setLastName(personDto.getLastNm());
-		personInfo.setParticipantId(personDto.getPtcpntId());
-		personInfo.setSocSecNo(personDto.getSsnNbr());
-		return personInfo;
-	}
-
 }
