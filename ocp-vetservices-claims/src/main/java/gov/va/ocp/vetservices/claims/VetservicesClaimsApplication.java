6518d0b4fd950ace880539d809dcf85267240aed
rajuthota <rajuthota@10.208.251.90>
2019-03-20 17:08:51 -0400

    "CMAPI2-127 => Added patterns for Entity/Domain/Provider"

--- ocp-vetservices-claims/src/main/java/gov/va/ocp/vetservices/claims/VetservicesClaimsApplication.java
+++ ocp-vetservices-claims/src/main/java/gov/va/ocp/vetservices/claims/VetservicesClaimsApplication.java
@@ -6,8 +6,11 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableAsync;
+
+import gov.va.ocp.framework.rest.provider.aspect.LogAnnotatedMethodRequestResponseAspect;
 
 @SpringBootApplication
 @EnableDiscoveryClient // needed to reach out to spring cloud config, eureka
@@ -20,5 +23,10 @@ public class VetservicesClaimsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VetservicesClaimsApplication.class, args);
+	}
+	
+	@Bean
+	LogAnnotatedMethodRequestResponseAspect myAspect() {
+		return new LogAnnotatedMethodRequestResponseAspect();
 	}
 }
