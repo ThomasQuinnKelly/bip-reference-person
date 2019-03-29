2dad17f3a46c7616fe9d7236754a51f08e8f1a9e
Raju Thota <raju_thota@hotmail.com>
2019-03-29 12:16:49 -0400

    CMAPI2-128 => Adding the ability to run oracle in docker mode for local-int profile

--- start-all-claims.sh
+++
@@ -1,4 +0,0 @@
-# Starts the openshift reference spring boot service, including consul, vault and redis
-
-docker-compose -f docker-compose-claims.yml \
-	up --build -d
--- stop-all-claims.sh
+++
@@ -1,4 +0,0 @@
-# Stops the entire platform, including all log aggregation services
-
-docker-compose -f docker-compose-claims.yml \
-	down -v
