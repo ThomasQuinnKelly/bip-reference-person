717a1744220d783a850dc8239db169efd792fc8f
varun-akula <vakula@meetveracity.com>
2019-04-08 18:25:11 -0400

    CMAPI2-184 Change Object[] to String[]

--- bip-reference-person/src/main/java/gov/va/bip/reference/person/exception/PersonServiceException.java
+++ bip-reference-person/src/main/java/gov/va/bip/reference/person/exception/PersonServiceException.java
@@ -26,7 +26,7 @@ public class PersonServiceException extends BipRuntimeException {
 	 *            <a href="https://tools.ietf.org/html/rfc7231">https://tools.ietf.org/html/rfc7231</a>
 	 * @param params - arguments to fill in any params in the MessageKey message (e.g. value for {0})
 	 */
-	public PersonServiceException(final MessageKey key, final MessageSeverity severity, HttpStatus status, Object... params) {
+	public PersonServiceException(final MessageKey key, final MessageSeverity severity, final HttpStatus status, final String... params) {
 		super(key, severity, status, params);
 	}
 
@@ -42,7 +42,7 @@ public class PersonServiceException extends BipRuntimeException {
 	 * @param cause - the throwable that caused this throwable
 	 * @param params - arguments to fill in any params in the MessageKey message (e.g. value for {0})
 	 */
-	public PersonServiceException(MessageKey key, MessageSeverity severity, HttpStatus status, Throwable cause, Object... params) {
+	public PersonServiceException(final MessageKey key, final MessageSeverity severity, final HttpStatus status, final Throwable cause, final String... params) {
 		super(key, severity, status, cause, params);
 	}
 }
