ad2bd2e9c06cbabca384d1cb7aa7b2572a975061
Abhijit Kulkarni <akulkarni@meetveracity.com>
2019-05-16 10:58:05 -0400

    Deleted bip-vetservices-claims subproject as it is in separate repo
    https://github.com/department-of-veterans-affairs/bip-vetservices-claims

--- bip-vetservices-claims/src/main/resources/import.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO CLAIM (ID,TYPE) VALUES(1,'526');
-insert into CLAIM (ID,TYPE) VALUES(2,'4142');
-
-insert into Attributes VALUES(1,'evss_claims','2018-06-04','false','true','false','2018-01-04','2017-06-04','true','true','Claim recieved','2018-07-30T17:31:15.958Z','false');
-insert into Attributes VALUES(2,'evss_claims','2011-06-04','false','true','false','2012-01-04','2014-06-04','true','true','Claim recieved','2019-01-30T17:31:15.958Z','false');
