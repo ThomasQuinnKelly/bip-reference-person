5676e571d75451cb0a85b535a581018a72efbfa7
Abhijit Kulkarni <akulkarni@meetveracity.com>
2019-05-21 09:20:53 -0400

    CMAPI2-248# Design First approach changes using openapi

--- bip-reference-person/src/main/java/gov/va/bip/reference/person/api/provider/PersonResource.java
+++ bip-reference-person/src/main/java/gov/va/bip/reference/person/api/provider/PersonResource.java
@@ -4,10 +4,10 @@ import javax.annotation.PostConstruct;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.actuate.health.Health;
-import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.boot.info.BuildProperties;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,13 +18,10 @@ import org.springframework.web.bind.annotation.RestController;
 import gov.va.bip.framework.log.BipLogger;
 import gov.va.bip.framework.log.BipLoggerFactory;
 import gov.va.bip.framework.swagger.SwaggerResponseMessages;
-import gov.va.bip.framework.transfer.ProviderTransferObjectMarker;
 import gov.va.bip.reference.person.api.ReferencePersonApi;
 import gov.va.bip.reference.person.api.model.v1.PersonInfoRequest;
 import gov.va.bip.reference.person.api.model.v1.PersonInfoResponse;
 import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
 
 /**
  * REST Person Service endpoint
@@ -33,7 +30,7 @@ import io.swagger.annotations.ApiResponses;
  *
  */
 @RestController
-public class PersonResource implements ReferencePersonApi, HealthIndicator, SwaggerResponseMessages {
+public class PersonResource implements ReferencePersonApi, SwaggerResponseMessages {
 
 	/** Logger instance */
 	private static final BipLogger LOGGER = BipLoggerFactory.getLogger(PersonResource.class);
@@ -57,25 +54,6 @@ public class PersonResource implements ReferencePersonApi, HealthIndicator, Swag
 	}
 
 	/**
-	 * A REST call to test this endpoint is up and running.
-	 * <p>
-	 * This endpoint is NOT intercepted by the standard publicServiceResponseRestMethod aspect
-	 * because the return type does not implement {@link ProviderTransferObjectMarker}.
-	 *
-	 * @see org.springframework.boot.actuate.health.HealthIndicator#health()
-	 */
-	@Override
-	@RequestMapping(value = URL_PREFIX + "/health", method = RequestMethod.GET)
-	@ApiOperation(value = "A health check of this endpoint",
-			notes = "Will perform a basic health check to see if the operation is running.")
-	@ApiResponses(value = {
-			@ApiResponse(code = 200, message = MESSAGE_200) })
-	public Health health() {
-		return Health.up().withDetail("Reference Person Service REST Endpoint", "Person Service REST Provider Up and Running!")
-				.build();
-	}
-
-	/**
 	 * Search for Person Information by their participant ID.
 	 * <p>
 	 * CODING PRACTICE FOR RETURN TYPES - Platform auditing aspects support two return types.
@@ -94,13 +72,13 @@ public class PersonResource implements ReferencePersonApi, HealthIndicator, Swag
 			produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.POST)
 	@ApiOperation(value = "Retrieve person information by PID from Person Service .",
 			notes = "Will return a person info object based on search by PID.")
-	public PersonInfoResponse personByPid(@Valid @RequestBody final PersonInfoRequest personInfoRequest) {
+	public ResponseEntity<PersonInfoResponse> personByPid(@Valid @RequestBody final PersonInfoRequest personInfoRequest) {
 		LOGGER.debug("personByPid() method invoked");
 
 		PersonInfoResponse providerResponse = serviceAdapter.personByPid(personInfoRequest);
 		// send provider response back to consumer
 		LOGGER.debug("Returning providerResponse to consumer");
-		return providerResponse;
+		return new ResponseEntity<>(providerResponse, HttpStatus.OK);
 	}
 
 	/**
