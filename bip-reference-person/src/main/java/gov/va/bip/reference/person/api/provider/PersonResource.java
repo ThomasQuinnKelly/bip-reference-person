cde0b54a1b6417cc58bab26ebd81eaf1bf7577ef
Abhijit Kulkarni <akulkarni@meetveracity.com>
2019-06-04 16:36:33 -0400

    CMAPI2-248# Modified messages, keys and OpenAPI specification

--- bip-reference-person/src/main/java/gov/va/bip/reference/person/api/provider/PersonResource.java
+++ bip-reference-person/src/main/java/gov/va/bip/reference/person/api/provider/PersonResource.java
@@ -6,13 +6,10 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.info.BuildProperties;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import gov.va.bip.framework.log.BipLogger;
@@ -21,7 +18,7 @@ import gov.va.bip.framework.swagger.SwaggerResponseMessages;
 import gov.va.bip.reference.person.api.ReferencePersonApi;
 import gov.va.bip.reference.person.api.model.v1.PersonInfoRequest;
 import gov.va.bip.reference.person.api.model.v1.PersonInfoResponse;
-import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 
 /**
  * REST Person Service endpoint
@@ -54,34 +51,6 @@ public class PersonResource implements ReferencePersonApi, SwaggerResponseMessag
 	}
 
 	/**
-	 * Search for Person Information by their participant ID.
-	 * <p>
-	 * CODING PRACTICE FOR RETURN TYPES - Platform auditing aspects support two return types.
-	 * <ol>
-	 * <li>An object that implements ProviderTransferObjectMarker, e.g.: PersonInfoResponse
-	 * <li>An object of type ResponseEntity&lt;ProviderTransferObjectMarker&gt;,
-	 * e.g. a ResponseEntity that wraps some class that implements ProviderTransferObjectMarker.
-	 * </ol>
-	 * The auditing aspect won't be triggered if the return type in not one of the above.
-	 *
-	 * @param personInfoRequest the person info request
-	 * @return the person info response
-	 */
-	@Override
-	@RequestMapping(value = URL_PREFIX + "/pid",
-			produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.POST)
-	@ApiOperation(value = "Retrieve person information by PID from Person Service .",
-			notes = "Will return a person info object based on search by PID.")
-	public ResponseEntity<PersonInfoResponse> personByPid(@Valid @RequestBody final PersonInfoRequest personInfoRequest) {
-		LOGGER.debug("personByPid() method invoked");
-
-		PersonInfoResponse providerResponse = serviceAdapter.personByPid(personInfoRequest);
-		// send provider response back to consumer
-		LOGGER.debug("Returning providerResponse to consumer");
-		return new ResponseEntity<>(providerResponse, HttpStatus.OK);
-	}
-
-	/**
 	 * Registers fields that should be allowed for data binding.
 	 *
 	 * @param binder
@@ -89,8 +58,38 @@ public class PersonResource implements ReferencePersonApi, SwaggerResponseMessag
 	 */
 	@InitBinder
 	public void initBinder(final WebDataBinder binder) {
-		binder.setAllowedFields(new String[] { "personInfo", "firstName", "lastName", "middleName", "fileNumber", "participantId",
-				"ssn" });
+		binder.setAllowedFields(new String[] { "personInfo", "firstName", "lastName", "middleName", "fileNumber",
+				"participantId", "ssn" });
+	}
+
+	/**
+	 * Search for Person Information by their participant ID.
+	 * <p>
+	 * CODING PRACTICE FOR RETURN TYPES - Platform auditing aspects support two
+	 * return types.
+	 * <ol>
+	 * <li>An object that implements ProviderTransferObjectMarker, e.g.:
+	 * PersonInfoResponse
+	 * <li>An object of type ResponseEntity&lt;ProviderTransferObjectMarker&gt;,
+	 * e.g. a ResponseEntity that wraps some class that implements
+	 * ProviderTransferObjectMarker.
+	 * </ol>
+	 * The auditing aspect won't be triggered if the return type in not one of
+	 * the above.
+	 *
+	 * @param personInfoRequest
+	 *            the person info request
+	 * @return the person info response
+	 */
+	@Override
+	public ResponseEntity<PersonInfoResponse> personByPid(
+			@ApiParam(value = "personInfoRequest", required = true) @Valid @RequestBody final PersonInfoRequest personInfoRequest) {
+		LOGGER.debug("personByPid() method invoked");
+
+		PersonInfoResponse providerResponse = serviceAdapter.personByPid(personInfoRequest);
+		// send provider response back to consumer
+		LOGGER.debug("Returning providerResponse to consumer");
+		return new ResponseEntity<>(providerResponse, HttpStatus.OK);
 	}
 
 }
