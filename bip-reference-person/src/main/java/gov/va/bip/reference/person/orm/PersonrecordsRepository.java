100e3b9565402c1d12b042ceb3374a7d827a60c6
varun-akula <vakula@meetveracity.com>
2019-07-15 13:03:26 -0400

    CMAPI2-216 Fix ORM issues

--- bip-reference-person/src/main/java/gov/va/bip/reference/person/orm/PersonrecordsRepository.java
+++ bip-reference-person/src/main/java/gov/va/bip/reference/person/orm/PersonrecordsRepository.java
@@ -4,8 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PersonrecordsRepository extends JpaRepository<Personrecord, Long> {
+public interface PersonRecordsRepository extends JpaRepository<PersonRecord, Long> {
 
-	Personrecord findByPid(long pid);
+	PersonRecord findByPid(long pid);
 
 }
