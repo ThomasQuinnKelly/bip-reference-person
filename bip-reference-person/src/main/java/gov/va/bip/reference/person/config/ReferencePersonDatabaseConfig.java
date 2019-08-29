d7a3d6360f984424e3f39b35f5daade2c0c533d4
armin <aburkholder@meetveracity.com>
2019-08-29 15:21:29 -0400

    454 prep multi-database

---
+++ bip-reference-person/src/main/java/gov/va/bip/reference/person/config/ReferencePersonDatabaseConfig.java
@@ -0,0 +1,12 @@
+package gov.va.bip.reference.person.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ReferencePersonDatabaseConfig {
+
+	public ReferencePersonDatabaseConfig() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
