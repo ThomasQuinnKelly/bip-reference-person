2b4765e247c02d46fe457ea68b4026347fcfc7bd
Abhijit Kulkarni <akulkarni@meetveracity.com>
2019-10-02 19:06:20 -0400

    AF-528# added sonar exclusions for person packages.
    Temporary solution, longer term we plan to add unit tests coverage

--- bip-reference-person/src/main/java/gov/va/bip/reference/person/config/PersonInfoDatasourceConfig.java
+++ bip-reference-person/src/main/java/gov/va/bip/reference/person/config/PersonInfoDatasourceConfig.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -78,6 +79,7 @@ public class PersonInfoDatasourceConfig extends PersonDatasourceBase {
 	 */
 	@Bean
 	@ConfigurationProperties(prefix = INFO_JPA_PREFIX)
+	@RefreshScope
 	public LocalContainerEntityManagerFactoryBean infoEntityManagerFactory(
 			EntityManagerFactoryBuilder builder,
 			@Qualifier("infoDataSource") DataSource dataSource) {
