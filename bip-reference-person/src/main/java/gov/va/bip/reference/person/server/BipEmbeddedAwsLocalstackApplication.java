e8d17bfbc45575ab13559b86277b3e4872e50538
Thomas Kelly <Kelly_Thomas@bah.com>
2020-01-17 12:16:57 -0800

    Attempt

--- bip-reference-person/src/main/java/gov/va/bip/reference/person/server/BipEmbeddedAwsLocalstackApplication.java
+++ bip-reference-person/src/main/java/gov/va/bip/reference/person/server/BipEmbeddedAwsLocalstackApplication.java
@@ -11,6 +11,7 @@ import com.amazonaws.services.sqs.model.CreateQueueRequest;
 import com.amazonaws.services.sqs.model.GetQueueAttributesRequest;
 import com.amazonaws.services.sqs.model.GetQueueAttributesResult;
 import com.amazonaws.services.sqs.model.QueueAttributeName;
+import gov.va.bip.framework.localstack.autoconfigure.LocalstackAutoConfiguration;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -35,51 +36,55 @@ import java.util.regex.PatternSyntaxException;
 @ConditionalOnProperty(name = "localstack.enabled", havingValue = "true")
 //@Profile(BipCommonSpringProfiles.PROFILE_EMBEDDED_AWS)
 //@EnableConfigurationProperties({ BipAwsLocalstackProperties.class, SqsProperties.class, SnsProperties.class })
-public class BipEmbeddedAwsLocalstackApplication {
+public class BipEmbeddedAwsLocalstackApplication extends LocalstackAutoConfiguration {
 
-//	@Autowired
-//	private SqsProperties sqsProperties;
-//
-//	@Autowired
-//	private SnsProperties snsProperties;
+	@Autowired
+	private SqsProperties sqsProperties;
+
+	@Autowired
+	private SnsProperties snsProperties;
+
+	@Override
+	public void configureAwsLocalStack() {
+		snsCreateTopicSubscribedToQueue();
+	}
 
 	private void snsCreateTopicSubscribedToQueue() {
 		AmazonSNS snsClient = TestUtils.getClientSNS();
 		AmazonSQS sqsClient = TestUtils.getClientSQS();
 
-//		// Create an Amazon SNS topic.
-//		final CreateTopicRequest createTopicRequest = new CreateTopicRequest(snsProperties.getTopic());
-//		final CreateTopicResult createTopicResult = snsClient.createTopic(createTopicRequest);
-//
-//		// Print the topic ARN.
-//		 String topicArn = createTopicResult.getTopicArn();
-//		 System.out.println("TopicArn:" + createTopicResult.getTopicArn());
-//
-//
-//		// Print the request ID for the CreateTopicRequest action.
-//		System.out.println("CreateTopicRequest: " + snsClient.getCachedResponseMetadata(createTopicRequest));
-//
-//		//create sqs que
-//		CreateQueueRequest createStandardQueueRequest = new CreateQueueRequest(sqsProperties.getQueueName());
-//		String queueURL = sqsClient.createQueue(createStandardQueueRequest).getQueueUrl();
-//
-//		//print the SQS queues
-//		System.out.println("SQS Queue: " +sqsClient.listQueues());
-//
-//		// Subscribe an email endpoint to an Amazon SNS topic.
-//		final SubscribeRequest subscribeRequest = new SubscribeRequest(topicArn, "sqs", queueURL);
-//		snsClient.subscribe(subscribeRequest);
-//
-//		// Publish a message to an Amazon SNS topic.
-//		final String msg = "If you receive this message, publishing a message to an Amazon SNS topic works.";
-//		final PublishRequest publishRequest = new PublishRequest(topicArn, msg);
-//		final PublishResult publishResponse = snsClient.publish(publishRequest);
-//
-//		// Print the MessageId of the message.
-//		System.out.println("MessageId: " + publishResponse.getMessageId());
-//
-//		//Print the SNS published message from the subscribed que
-//		sqsClient.receiveMessage(queueURL).getMessages().forEach(System.out::println);
+		// Create an Amazon SNS topic.
+		final CreateTopicRequest createTopicRequest = new CreateTopicRequest(snsProperties.getTopic());
+		final CreateTopicResult createTopicResult = snsClient.createTopic(createTopicRequest);
+
+		// Print the topic ARN.
+	 	String topicArn = createTopicResult.getTopicArn();
+	 	System.out.println("TopicArn:" + createTopicResult.getTopicArn());
+
+		// Print the request ID for the CreateTopicRequest action.
+		System.out.println("CreateTopicRequest: " + snsClient.getCachedResponseMetadata(createTopicRequest));
+
+		//create sqs que
+		CreateQueueRequest createStandardQueueRequest = new CreateQueueRequest(sqsProperties.getQueueName());
+		String queueURL = sqsClient.createQueue(createStandardQueueRequest).getQueueUrl();
+
+		//print the SQS queues
+		System.out.println("SQS Queue: " +sqsClient.listQueues());
+
+		// Subscribe an email endpoint to an Amazon SNS topic.
+		final SubscribeRequest subscribeRequest = new SubscribeRequest(topicArn, "sqs", queueURL);
+		snsClient.subscribe(subscribeRequest);
+
+		// Publish a message to an Amazon SNS topic.
+		final String msg = "If you receive this message, publishing a message to an Amazon SNS topic works.";
+		final PublishRequest publishRequest = new PublishRequest(topicArn, msg);
+		final PublishResult publishResponse = snsClient.publish(publishRequest);
+
+		// Print the MessageId of the message.
+		System.out.println("MessageId: " + publishResponse.getMessageId());
+
+		//Print the SNS published message from the subscribed que
+		sqsClient.receiveMessage(queueURL).getMessages().forEach(System.out::println);
 
 	}
 
