6a03e524c5c8b5297e3c0b8395cb11ae94f60e4e
Thomas Kelly <Kelly_Thomas@bah.com>
2020-01-21 09:13:25 -0500

    Clean up

--- bip-reference-person/src/main/java/gov/va/bip/reference/person/server/BipEmbeddedAwsLocalstackApplication.java
+++ bip-reference-person/src/main/java/gov/va/bip/reference/person/server/BipEmbeddedAwsLocalstackApplication.java
@@ -1,41 +1,20 @@
 package gov.va.bip.reference.person.server;
 
-import cloud.localstack.Localstack;
 import cloud.localstack.TestUtils;
-import cloud.localstack.docker.DockerExe;
-import cloud.localstack.docker.annotation.LocalstackDockerConfiguration;
 import com.amazonaws.services.sns.AmazonSNS;
 import com.amazonaws.services.sns.model.*;
 import com.amazonaws.services.sqs.AmazonSQS;
 import com.amazonaws.services.sqs.model.CreateQueueRequest;
-import com.amazonaws.services.sqs.model.GetQueueAttributesRequest;
-import com.amazonaws.services.sqs.model.GetQueueAttributesResult;
-import com.amazonaws.services.sqs.model.QueueAttributeName;
 import gov.va.bip.framework.localstack.autoconfigure.LocalstackAutoConfiguration;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.util.CollectionUtils;
-
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.regex.PatternSyntaxException;
 
 /**
  * This class will configure AWS localstack services.
  */
 @Configuration
 @ConditionalOnProperty(name = "localstack.enabled", havingValue = "true")
-//@Profile(BipCommonSpringProfiles.PROFILE_EMBEDDED_AWS)
-//@EnableConfigurationProperties({ BipAwsLocalstackProperties.class, SqsProperties.class, SnsProperties.class })
 public class BipEmbeddedAwsLocalstackApplication extends LocalstackAutoConfiguration {
 
 	@Autowired
@@ -64,7 +43,7 @@ public class BipEmbeddedAwsLocalstackApplication extends LocalstackAutoConfigura
 		// Print the request ID for the CreateTopicRequest action.
 		System.out.println("CreateTopicRequest: " + snsClient.getCachedResponseMetadata(createTopicRequest));
 
-		//create sqs que
+		//create sqs queue
 		CreateQueueRequest createStandardQueueRequest = new CreateQueueRequest(sqsProperties.getQueueName());
 		String queueURL = sqsClient.createQueue(createStandardQueueRequest).getQueueUrl();
 
@@ -83,7 +62,7 @@ public class BipEmbeddedAwsLocalstackApplication extends LocalstackAutoConfigura
 		// Print the MessageId of the message.
 		System.out.println("MessageId: " + publishResponse.getMessageId());
 
-		//Print the SNS published message from the subscribed que
+		//Print the SNS published message from the subscribed queue
 		sqsClient.receiveMessage(queueURL).getMessages().forEach(System.out::println);
 
 	}
