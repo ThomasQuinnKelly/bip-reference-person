b7cdd499ab126ce684733bfb744ce428fc91d39a
Maxim Moldenhauer <moldenhauer_maxim@bah.com>
2020-04-10 07:34:08 -0400

    AF-1031: Touch BIP Reference Person

--- bip-reference-person/src/main/java/gov/va/bip/reference/person/HomeController.java
+++ bip-reference-person/src/main/java/gov/va/bip/reference/person/HomeController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 /**
- * Controller for the root URL - redirects to the Swagger UI
+ * Controller for the root URL - redirects to the Swagger UI.
  */
 @Controller
 public class HomeController {
